86b4057b3d24ef4ac946a5c0be8cdc17ea0ccc68
kilfu0701 <kilfu0701@gmail.com>
2016-09-14 14:21:42 +0900

    fix date format error

--- lib/jekyll-multiple-lang/version.rb
+++ lib/jekyll-multiple-lang/version.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module MultipleLang
     VERSION = "1.0.2"
-    RELEASES = "2016/09/14"
+    RELEASES = "2016-09-14"
     AUTHOR = "kilfu0701"
     HOMEPAGE = "https://github.com/kilfu0701/jekyll-multiple-lang"
   end
